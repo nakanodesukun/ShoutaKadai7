c30cae18fbc3028cdd612c95f9be5d2ea175fc84
nakanodesukun <94818124+nakanodesukun@users.noreply.github.com>
2022-01-01 22:17:59 +0900

    MVCアーキテクチャを意識してみました

--- 7/Modal/Modal.swift
+++ 7/Modal/Modal.swift
@@ -6,31 +6,44 @@
 //
 
 import Foundation
+import UIKit
 
+enum caluculateError : Error {
+case nonAdded //"たされる数に数値を入力してください"
+case nonAdd //たす数に数値を入力して下さい
+    case minuend //引かれる数
+    case subtract //引くかず
+    //コンピューテッドプロパティ
+    var massage : String {
+        switch self {
+        case .nonAdded: return "たされる数に数値を入力して下さい"
+        case .nonAdd:   return   "たす数に数値を入力して下さい"
+        case .minuend:  return  "引かれる数を入力して下さい"
+        case .subtract: return "引く数を入力して下さい"
+        
+        }
+    }
+   
+}
+final class Modal {
+    func addCalculate(text1: String, text2: String) throws -> Int {
+        guard let value1 = Int(text1) else {
+            throw caluculateError.nonAdded
+        }
+        guard let value2 = Int(text2) else {
+            throw caluculateError.nonAdd
+        }
+        return value1 + value2
+    }
+    func calculate(text1: String, text2: String)throws -> Int {
+        guard let value1 = Int(text1) else {
+            throw caluculateError.minuend
+        }
+        guard let value2 = Int(text2) else {
+            throw caluculateError.subtract
+        }
+        return value1 - value2
+    }
+    
+}
 
-//足し算クラス
-// final class  Modal {
-//    func addCalculate(text1: String, text2: String) -> Int {
-//        guard let value1 = Int(text1) else {
-//            print("1の値をINT型に変換できませんでした")
-//            return 0
-//        }
-//        guard let value2 = Int(text2) else {
-//            print("２の値をINT型に変換できませんでした")
-//            return 0
-//        }
-//        return value1 + value2
-//    }
-//引き算クラス
-//    func subtrctionCalculate(text1: String, text2: String) -> Int {
-//        guard let value1 = Int(text1) else {
-//            print("1の値をINT型に変換できませんでした")
-//            return 0
-//        }
-//        guard let value2 = Int(text2) else {
-//            print("２の値をINT型に変換できませんでした")
-//            return 0
-//        }
-//        return value1 - value2
-//    }
-//}
