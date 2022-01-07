4648fab448dc1ce57cb7a294f6b7206231241fc7
nakanodesukun <94818124+nakanodesukun@users.noreply.github.com>
2022-01-07 23:44:14 +0900

    更新しました

--- 7/Controllers/SubtractionViewController.swift
+++ 7/Controllers/SubtractionViewController.swift
@@ -16,6 +16,7 @@ class GreenViewController: UIViewController {
     @IBOutlet private weak var resultLabel: UILabel!
 
     @IBAction func subtractionButton(_ sender: Any) {
+        //　ローカル定数と呼ぶ？それともメソッド内の定数と呼ぶ？
         let resulutText: String?
         do {
             let result = try calculator.subtactionCalculate(text1: firstTextField.text ?? "", text2: secondTextField.text ?? "")
